7cad97f334cfdf65d9ad69b7613760f6b905ae4f
pumkko <c.coque@hotmail.fr>
2024-01-04 11:56:13 -0500

    add get endpoint before i try to refacto this stuff

--- packages/functions/src/ValibotSchema.ts
+++ packages/functions/src/ValibotSchema.ts
@@ -1,6 +1,10 @@
-import { endsWith, minLength, nonNullish, object, optional, required, string, safeParse} from "valibot";
+import { endsWith, minLength, nonNullish, object, optional, required, string, safeParse, uuid } from "valibot";
 
 export const CreateNoteSchema = object({
-    content:  nonNullish(string([minLength(3)])),
+    content: nonNullish(string([minLength(3)])),
     attachment: string([endsWith('.jpg', "Must be a JPG file")])
 });
+
+export const GetNoteSchema = object({
+    id: string([uuid()])
+})
---
+++ packages/functions/src/get.ts
@@ -0,0 +1,51 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { ApiHandler } from "sst/node/api";
+import { safeParse } from "valibot";
+import { GetNoteSchema } from "./ValibotSchema";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { Table } from "sst/node/table";
+
+
+const dynamoDb = new DynamoDBClient();
+
+export const handler = ApiHandler(async (event) => {
+
+    const parseResult = safeParse(GetNoteSchema, event.pathParameters);
+
+    if (!parseResult.success) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify(parseResult.issues)
+        }
+    }
+
+    const client = DynamoDBDocument.from(dynamoDb);
+
+    try {
+        const response = await client.get({
+            Key: {
+                userId: '123',
+                noteId: parseResult.output.id
+            },
+            TableName: Table.Notes.tableName
+        });
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.Item)
+        }
+    } catch (error: unknown) {
+        let message;
+        if (error instanceof Error) {
+            message = error.message;
+        } else {
+            message = String(error);
+        }
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: message }),
+        };
+    }
+
+
+
+})
