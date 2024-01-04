9de2a68ff4899d9ee5439828520c0794535ae36b
pumkko <c.coque@hotmail.fr>
2024-01-04 16:12:30 -0500

    Add routing

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./container/Home";
+import { NotFound } from "./NotFound";
 
 export function RouterOutlet() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />;
         </Routes>
     )
 }
