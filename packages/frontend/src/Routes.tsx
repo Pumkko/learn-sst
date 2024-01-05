dfcc7df86f10afed6265c08c9da7b8b30c7b0c08
pumkko <c.coque@hotmail.fr>
2024-01-05 13:40:19 -0500

    Add login page

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./container/Home";
 import { NotFound } from "./notFound/NotFound";
+import { Login } from "./container/Login";
 
 export function RouterOutlet() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="*" element={<NotFound />} />;
+            <Route path="*" element={<NotFound />} />
+            <Route path="/login" element={<Login />}/>
         </Routes>
     )
 }
